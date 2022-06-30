4e1a3df05c71b0b589eaee2c50c4813068752846
Luigi Matera <luigi.matera@studio.unibo.it>
2022-06-30 16:41:08 +0200

    graphics updated

--- graphics.py
+++ graphics.py
@@ -3,6 +3,11 @@ from contextlib import contextmanager
 
 @contextmanager
 def highlighted_cyan_text():
+    """
+    Note
+    ----
+    This function will highlight an output written in the console
+    """
     print('\033[0;36m', end='')
     try:
         yield
