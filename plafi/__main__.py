bb08d11791fc809ac39d686b3d8472ddc4ea5011
Luigi Matera <luigi.matera@studio.unibo.it>
2022-07-01 10:20:32 +0200

    main updated

--- plafi/__main__.py
+++ plafi/__main__.py
@@ -1,8 +1,11 @@
 import argparse
 from . import functions as fc
 import os
+
+# Not showing the traceback in case of raising error
 import sys
 sys.tracebacklimit = 0
+
 
 def main():
     # parser initialization
@@ -10,12 +13,12 @@ def main():
     subparsers = parser.add_subparsers(
         help='possible actions', dest='subparser')
 
-    # PLOT argument
+    # PLOTTING argument
     plot_parser = subparsers.add_parser('plot', help='plot the data')
     plot_parser.add_argument("path", help='path to data to plot', type=str, nargs="?")
     plot_parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
 
-    # FIT argument
+    # FITTING argument
     fit_parser = subparsers.add_parser('fit', help='fit the data')
 
     # CONSTANTS argument
@@ -45,17 +48,20 @@ def main():
                     raise ValueError("It was not possible to read the file")
 
     # fit case
-    if args.subparser == 'fit':
+    elif args.subparser == 'fit':
         fc.fitting_procedure()
 
     # const case
-    if args.subparser == 'const':
-        fc.initialize_constants()
+    elif args.subparser == 'const':
         if args.add:
             fc.add_constant()
         if args.delete:
             fc.delete_constant()
         fc.print_constants()
+
+    # A message will appear in case the application is called without any option
+    else:
+        print("Type 'plafi -h' to receive some help.")
 
 
 if __name__ == '__main__':
